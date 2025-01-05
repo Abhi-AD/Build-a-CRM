70b7627e8068c8cdb48a205d7c399209c64f4bfb
Abhi-AD <dangiabhi332@gmail.com>
2025-01-06 00:55:50 +0545

    add the auth perimission and signals add the leads

--- apps/leads/admin.py
+++ apps/leads/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from apps.leads.models import User, Lead, Agent
+from apps.leads.models import User, Lead, Agent, UserProfile
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Lead)
 admin.site.register(Agent)
+admin.site.register(UserProfile)
