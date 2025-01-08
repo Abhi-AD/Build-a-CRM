8f3097a2c17f1c3f7792eea165241d22d6887b37
Abhi-AD <dangiabhi332@gmail.com>
2025-01-08 19:56:37 +0545

    Add the crisy form in this project and styling add

--- Build_a_CRM/settings.py
+++ Build_a_CRM/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     # custom apps
     "apps.leads",
     "apps.agents",
+    # third-party apps
+    "crispy_forms",
+    "crispy_tailwind",
 ]
 
 MIDDLEWARE = [
@@ -141,3 +144,6 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 LOGIN_REDIRECT_URL = "/leads/"
 LOGIN_URL = "/login"
 LOGOUT_REDIRECT_URL = "/login/"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
